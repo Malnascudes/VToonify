0ab49dc5efd1a1baa50e7bfa83a4526d73504995
Daniel <daniel@Daniels-MacBook-Pro.local>
2023-11-25 13:57:14 +0100

    Fixing the comment

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -7,7 +7,7 @@
 
 # wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O FILENAME && rm -rf /tmp/cookies.txt
 
-# PSP encoder checkpoints
+# Getting checkpoints zip stored in Crypsis drive
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1OYwpYYise3JUdwFL9x1UuHodkdDsG1oz' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1OYwpYYise3JUdwFL9x1UuHodkdDsG1oz" -O checkpoint.zip && rm -rf /tmp/cookies.txt
 
 unzip checkpoint.zip
